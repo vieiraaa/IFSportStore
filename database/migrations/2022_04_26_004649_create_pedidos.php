f95c665cdff8e92146d4e58754483e564ab85328
nrigon <natarigonsc@gmail.com>
2022-04-29 23:31:42 -0300

    tabeslapedprod

---
+++ database/migrations/2022_04_26_004649_create_pedidos.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->string('status',2);
+            $table->date('dataped');
+            $table->date('dataent');
+            $table->unsignedBigInteger('cliente_id');
+            $table->unsignedBigInteger('endereco_id');
+            $table->unsignedBigInteger('transportadora_id');
+            $table->foreign('cliente_id')->references('id')->on('clientes');
+            $table->foreign('endereco_id')->references('id')->on('enderecos');
+            $table->foreign('transportadora_id')->references('id')->on('transportadoras');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+};
